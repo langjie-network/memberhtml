9f5d827fc61a686f13c0e48ce7d7e308ed816a68
jihefu <1744264136@qq.com>
2024-10-28 12:55:59 +0800

    车辆使用记录

--- pages/vehicle_regist/server.js
+++ pages/vehicle_regist/server.js
@@ -1,21 +1,9 @@
 
-app.get("/notiPost_fromCenterList",async (req,res)=>{
-    const r= await M.request.get("/home/notiPost/fromCenterList");
+app.get("/vehicleRegistGetList",async (req,res)=>{
+    const r= await M.request.get("/member_ajax/vehicleRegist/getList",{
+        page:1,
+        num:10
+    });
     res.send(r);
 })
 
- app.get("/notiPost_fromCenterUpdate",async (req,res)=>{
-     const {id,noti_client_mailId,atReply}=req.params;
-     const r=await M.request.put("/home/notiPost/fromCenterUpdate",{
-         "id":id,
-         "noti_client_mailId":noti_client_mailId,
-         "vote":"已阅",
-         "atReply":atReply||""
-     });
-     res.send(r);
-})
-app.get("/home_msgBox_list",async (req,res)=>{
-    const {page,num}=req.params;
-    const r=await M.request.get(`/home/msgBox/list?page=${page}&num=${num}`,{})
-    res.send(r);
-})
