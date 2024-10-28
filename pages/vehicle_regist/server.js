b65f1dc071a165f9b172f6478ce16fd3dc11d13a
jihefu <1744264136@qq.com>
2024-10-28 13:02:05 +0800

    分页

--- pages/vehicle_regist/server.js
+++ pages/vehicle_regist/server.js
@@ -2,7 +2,8 @@
 app.get("/vehicleRegistGetList",async (req,res)=>{
     const r= await M.request.get("/member_ajax/vehicleRegist/getList",{
         page:1,
-        num:10
+        num:10,
+        filter:req.params.filter
     });
     res.send(r);
 })
