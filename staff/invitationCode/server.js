d1377328fb1e8f416d7b9078c119a4bef2c0f722
jihefu <1744264136@qq.com>
2025-03-10 13:21:43 +0800

    test01

--- staff/invitationCode/server.js
+++ staff/invitationCode/server.js
@@ -1,4 +1,24 @@
 app.get("/myInvitationMember",async (req,res)=>{
    const r= await M.request.get("/m/staff/myInvitationMember");
    res.send(r);
+});
+
+
+app.get("/pendingCompanyAndNameInfo",async (req,res)=>{
+   const user_id=req.params.user_id;
+   const r= await M.request.get("/m/staff/pendingCompanyAndNameInfo",{
+      user_id:user_id
+   });
+   res.send(r);
+});
+
+
+app.get("/updatePendingCompanyAndName",async (req,res)=>{
+   const {user_id,name,pending_company}=req.params;
+   const r= await M.request.post("/m/staff/updatePendingCompanyAndName",{
+      user_id,
+      name,
+      pending_company
+   });
+   res.send(r);
 })
