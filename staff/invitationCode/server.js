3a55fb4b4dc18c45cfaec6bab53ad7c856d4c4d2
PC <934031452@qq.com>
2025-03-16 20:55:47 +0800

    test01

--- staff/invitationCode/server.js
+++ staff/invitationCode/server.js
@@ -13,6 +13,18 @@ app.get("/pendingCompanyAndNameInfo",async (req,res)=>{
 });
 
 
+app.get("/changeIsPrizeSend",async (req,res)=>{
+   const user_id=req.params.user_id;
+   const is_prize_send=req.params.is_prize_send;
+   const r= await M.request.post("/m/staff/changeIsPrizeSend",{
+        user_id:user_id,
+        is_prize_send:is_prize_send
+   });
+   res.send(r);
+});
+
+
+
 app.get("/updatePendingCompanyAndName",async (req,res)=>{
    const {user_id,name,pending_company}=req.params;
    const r= await M.request.post("/m/staff/updatePendingCompanyAndName",{
